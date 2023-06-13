987a47b0f16695712722bf1da15dc889c886a7da
impati <impati0716@icloud.com>
2023-06-13 20:31:57 +0900

    first commit

---
+++ src/main/java/com/example/impatiboard/ImpatiBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.impatiboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpatiBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImpatiBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/impatiboard/ImpatiBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.impatiboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImpatiBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
